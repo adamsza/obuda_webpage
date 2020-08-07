689c30fe744003fb0e8a1ca92b4ab7668833e072
Adam Szaloky <szaloada@gmail.com>
2020-08-07 12:07:51 +0200

    Updates

---
+++ precache-manifest.219130f939d4ffea80496eb6d9d343e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58737f669802172fd18b4198a4e3a440",
+    "url": "/obuda_webpage/index.html"
+  },
+  {
+    "revision": "66f8045ff36012e418e5",
+    "url": "/obuda_webpage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9165b2d12ce6884f6ec0",
+    "url": "/obuda_webpage/static/js/2.d9d853b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/obuda_webpage/static/js/2.d9d853b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66f8045ff36012e418e5",
+    "url": "/obuda_webpage/static/js/main.454693bb.chunk.js"
+  },
+  {
+    "revision": "5ea403fc06150b8b8f54",
+    "url": "/obuda_webpage/static/js/runtime-main.1b1c3a37.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/obuda_webpage/static/media/logo.5d5d9eef.svg"
+  }
+]);
